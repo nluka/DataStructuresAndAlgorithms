9058955d67711adac16eed54d035243c8480a7d9
Nicholas Lukasevich <n.luka784@gmail.com>
2021-04-16 08:28:29 -0400

    Create HashTableImplementation.js

---
+++ HashTableImplementation.js
@@ -0,0 +1,36 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const address = this._hash(key);
+    if (this.data[address] === undefined) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+  }
+
+  get(key) {
+    const address = this._hash(key);
+    for (let i = 0; i < this.data[address].length; i++) {
+      if (this.data[address][i][0] === key) {
+        return this.data[address][i][1];
+      }
+    }
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.get('grapes');
+myHashTable.set('apples', 9);
+myHashTable.get('apples');
