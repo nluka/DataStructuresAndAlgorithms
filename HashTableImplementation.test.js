a6193731c979eadef3f22e4e8e5d76607f195331
Nicholas Lukasevich <n.luka784@gmail.com>
2021-04-23 13:17:04 -0400

    Update HashTableImplementation.test.js

--- HashTableImplementation.test.js
+++ HashTableImplementation.test.js
@@ -4,7 +4,7 @@ const util = require('./util.js');
 const HashTable = hashTableImplementation.HashTable;
 const getRandomAlphabeticalString = util.getRandomAlphabeticalString;
 
-test('1. HashTable.data.length should be the same length as the size passed into constructor', () => {
+test('0. HashTable.data.length should be the same length as the size passed into constructor', () => {
   for (let i = 0; i < 5; i++) {
     const SIZE = i * 10;
     const hashTable = new HashTable(SIZE);
@@ -12,7 +12,7 @@ test('1. HashTable.data.length should be the same length as the size passed into
   }
 });
 
-test('2. HashTable.data elements should be undefined when instantiated', () => {
+test('1. HashTable.data elements should be undefined when instantiated', () => {
   const SIZE = 10;
   const hashTable = new HashTable(SIZE);
   for (let i = 0; i < SIZE; i++) {
@@ -20,7 +20,7 @@ test('2. HashTable.data elements should be undefined when instantiated', () => {
   }
 });
 
-test('3. HashTable._hash should return number from zero to (size-1)', () => {
+test('2. HashTable._hash should return number from zero to (size-1)', () => {
   const SIZE = 10;
   const hashTable = new HashTable(SIZE);
   const NUMBER_OF_RUNS = 1_000;
@@ -32,7 +32,7 @@ test('3. HashTable._hash should return number from zero to (size-1)', () => {
   }
 });
 
-test('4. HashTable.set(key, value) should append [key, value] to data[hash]', () => {
+test('3. HashTable.set(key, value) should append [key, value] to data[hash]', () => {
   const SIZE = 10;
   const hashTable = new HashTable(SIZE);
   hashTable.set('apples', 15);
@@ -40,7 +40,7 @@ test('4. HashTable.set(key, value) should append [key, value] to data[hash]', ()
   expect(hashTable.data[address]).toEqual([['apples', 15]]);
 });
 
-test('5. HashTable.get(key) should return the set value', () => {
+test('4. HashTable.get(key) should return the set value', () => {
   const SIZE = 10;
   const hashTable = new HashTable(SIZE);
   const KEY = 'apples';
