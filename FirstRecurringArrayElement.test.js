fe5425e4d9e9ad3ae2f19cc6385ff5c1f9cf991f
Nicholas Lukasevich <n.luka784@gmail.com>
2021-04-23 13:07:00 -0400

    Update FirstRecurringArrayElement.test.js

--- FirstRecurringArrayElement.test.js
+++ FirstRecurringArrayElement.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@jest/globals');
 const { firstRecurringArrayElement } = require('./FirstRecurringArrayElement');
 
-test('firstRecurringArrayElement should return first recurring array element', () => {
+test('1. firstRecurringArrayElement should return first recurring array element', () => {
   expect(firstRecurringArrayElement([2, 5, 1, 2, 3, 5, 1, 2, 4])).toBe(2);
   expect(firstRecurringArrayElement([2, 1, 1, 2, 3, 5, 1, 2, 4])).toBe(1);
   expect(firstRecurringArrayElement([2, 5.5, 5.5, 2, 3, 5, 1, 2, 4])).toBeCloseTo(5.5, 2);
@@ -10,7 +10,7 @@ test('firstRecurringArrayElement should return first recurring array element', (
   expect(firstRecurringArrayElement([undefined, 1, undefined])).toBe(undefined);
 });
 
-test('firstRecurringArrayElement should return undefined when passed empty array or not an array', () => {
+test('2. firstRecurringArrayElement should return undefined when passed empty array or not an array', () => {
   expect(firstRecurringArrayElement([])).toBe(undefined);
   expect(firstRecurringArrayElement({})).toBe(undefined);
   expect(firstRecurringArrayElement(1)).toBe(undefined);
@@ -20,12 +20,12 @@ test('firstRecurringArrayElement should return undefined when passed empty array
   expect(firstRecurringArrayElement([{ 1: 'one' }, { 2: 'two' }, { 1: 'one' }])).toBe(undefined);
 });
 
-test('firstRecurringArrayElement should return undefined if all elements are unique', () => {
+test('3. firstRecurringArrayElement should return undefined if all elements are unique', () => {
   expect(firstRecurringArrayElement([2])).toBe(undefined);
   expect(firstRecurringArrayElement(['two'])).toBe(undefined);
 });
 
-test('firstRecurringArrayElement should not mutate passed in array', () => {
+test('4. firstRecurringArrayElement should not mutate passed in array', () => {
   const originalArray = [1, 2, 3];
   firstRecurringArrayElement(originalArray);
   expect(originalArray).toEqual([1, 2, 3]);
